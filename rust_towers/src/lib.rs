d2bc12bebca59cfec4f99323ac5b4fd771063589
Chris Couzens <ccouzens@gmail.com>
2021-07-12 22:29:12 +0100

    Try and do more in rust
    convert to slices whenever we can, and work with them.
    
    I don't think I can use Rust's Vec for memory allocations. Allocating is
    fine, but I think realloc and free both require the allocator to
    remember how much memory was assigned. Recreating a Vec from a pointer
    requires the length.

--- rust_towers/src/lib.rs
+++ rust_towers/src/lib.rs
@@ -1,14 +1,10 @@
-// #[no_mangle]
-// pub unsafe extern "C" fn bin2hex(input: *const u8, input_length: usize) {
-
-// }
-
-use libc::{c_char, c_int, c_uchar, c_void, free, malloc, realloc, size_t, strcpy, strlen};
+use libc::{c_char, c_int, c_uchar, c_void, free, malloc, realloc, size_t};
+use std::ffi::CStr;
 use std::mem::size_of;
 
 #[no_mangle]
-pub unsafe extern "C" fn smalloc(size: size_t) -> *mut c_void {
-    malloc(size)
+pub extern "C" fn smalloc(size: size_t) -> *mut c_void {
+    unsafe { malloc(size) }
 }
 
 #[no_mangle]
@@ -23,26 +19,26 @@ pub unsafe extern "C" fn srealloc(p: *mut c_void, size: size_t) -> *mut c_void {
     if !p.is_null() {
         realloc(p, size)
     } else {
-        malloc(size)
+        smalloc(size)
     }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn dupstr(s: *const c_char) -> *mut c_char {
-    let r = smalloc(1 + strlen(s)) as *mut c_char;
-    strcpy(r, s);
-    r
+    let s = CStr::from_ptr(s).to_bytes_with_nul();
+    let r = snewn::<u8>(s.len());
+    s.iter().zip(r.iter_mut()).for_each(|(&a, b)| *b = a);
+    r.as_mut_ptr() as *mut c_char
 }
 
-pub unsafe fn snewn<T>(number: size_t) -> *mut T {
-    smalloc(number * size_of::<T>()) as *mut T
+pub fn snewn<T>(number: size_t) -> &'static mut [T] {
+    unsafe { std::slice::from_raw_parts_mut(smalloc(number * size_of::<T>()) as *mut T, number) }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn bin2hex(input: *const c_uchar, inlen: c_int) -> *mut c_char {
     let input = std::slice::from_raw_parts::<c_uchar>(input, inlen as usize);
-    let retmem = snewn::<c_char>((inlen * 2 + 1) as size_t);
-    let output = std::slice::from_raw_parts_mut::<c_char>(retmem, (inlen * 2 + 1) as usize);
+    let output = snewn::<c_char>((inlen * 2 + 1) as size_t);
     for (p, i) in output.iter_mut().zip(0..inlen * 2) {
         let mut v = input[i as usize / 2];
         if i % 2 == 0 {
@@ -51,5 +47,5 @@ pub unsafe extern "C" fn bin2hex(input: *const c_uchar, inlen: c_int) -> *mut c_
         *p = b"0123456789abcdef"[v as usize & 0xF] as c_char;
     }
     output[inlen as usize * 2] = b'\0' as c_char;
-    retmem
+    output.as_mut_ptr()
 }
